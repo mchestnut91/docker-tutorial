b574d0bb41647ce97e4a025a7d83c0ec7826ca60
Michael Chestnut <mchestnu@gmail.com>
2020-01-29 17:55:52 -0500

    feature branch change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO MY NAME IS NOT MICHAEL
+          HELLO MY NAME IS NOT MICHAEL. I am on the feature branch
         </p>
         <a
           className="App-link"
